797bea0f8f9a0d568575d895ed2195da98244cd3
Shivansh038 <142441345+Shivansh038@users.noreply.github.com>
2024-04-07 11:03:55 +0530

    A garbage management system that alerts authorities and people when a garbage bin is about to overflow
    simulator takes hypothetical garbage capacity values. If it is more than the  threshold value, it gives a beep, and a red signal.
    
    Location is file containing class location which contains the bins in that location as objects and the sounds et as its methods.

--- location.py
+++ location.py
@@ -1,21 +1,34 @@
+import winsound
+import time
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 class Location:
     
-    def __init__(self,number,tablename):
+    def __init__(self,number):
         self.number=number
-        self.tablename=tablename
         
-
+    
     def signal(self):
-        print("red")
+        duration=1000
+        frequency=1000
+        num_beeps=7
+        for _ in range(num_beeps):
+            winsound.Beep(frequency,duration)
+            time.sleep(1)
+        
 
     def email(self):
-        print("empty bins budhu")
+        sendgridapi='639f83ce8d349fae368607d3446f15b4-6fbb86a2-d303-4b36-b8bb-62b267be6ced'
+        message=Mail(from_email="cyberbrigade.wms@gmail.com",to_emails='kaashvi.mail21@gmail.com',subject="Garbage Bin Full Alert",html_content='<strong>Garbage can is full and needs to be emptied<\strong>')
+        try: 
+            sg=SendGridAPIClient(sendgridapi)
+            response=sg.send(message)
+            print("Email sent successfully")
+        except Exception as e:
+            print("Email not sent")        
 
-    def sound(self):
-        print("beep")
 
-    def capacity(self):
-        print("hiii")
-        
 
     
--- simulator.py
+++ simulator.py
@@ -7,41 +7,59 @@ mydb=mc.connect(host='localhost',user='root',passwd='password')
 curse=mydb.cursor(buffered=True)
 curse.execute('use hackaccino')
 
-min_garbage=0
-max_garbage=100
+no_of_bins=3
+def garbage_fill():
+    for i in range(1,no_of_bins+1):
+        print("bin ",i)
+        min_garbage=0
+        max_garbage=100
+        threshold=95
+        num=str(i)
+        table="bin"+num
+        globals()["bin_"+str(i)]=Location(i)
+        q = "CREATE TABLE {} (Time varchar(255) Primary key, Capacity int)".format(table)  # Corrected query
+        #curse.execute(q)
+    
+        while True:
+            garbage_level = random.randint(min_garbage, max_garbage)
+            current_time = time.ctime()
+            garbage_time = str(current_time)
+            x= "INSERT INTO {} (Time, Capacity) VALUES ('{}', {})".format(table, garbage_time, garbage_level)
+            #curse.execute(x)
+            print(current_time, garbage_level)
+            min_garbage = garbage_level
+            if garbage_level > threshold:
+                globals()["bin_" + str(i)].signal()
+                print("RED LIGHT")
+                print("LID HAS BEEN CLOSED")
+                #globals()["bin_" + str(i)].email()
+                break
 
-no_of_bins=25
-for i in range(1,no_of_bins+1):
-    print("bin ",i)
-    min_garbage=0
-    max_garbage=100
-    threshold=95
-    j=1
-    table="bin"+str(i)
-    globals()["bin_"+str(i)]=Location(i,table)
-    tablenaam=globals()["bin_"+str(i)].tablename
-    #curse.execute("create table {}(Time varchar(100),Capacity integer)".format(tablenaam))
-    
-    while True:
-        garbage_level=random.randint(min_garbage,max_garbage)
-        current_time=time.ctime()
-        garbage_time=str(current_time)
-        #curse.execute("insert into {} values({},{})".format(tablenaam,garbage_time,garbage_level))
-        print(current_time,garbage_level)
-        min_garbage=garbage_level
-        if garbage_level>threshold:
-            globals()["bin_"+str(i)].signal()
-            break
-            
-        time.sleep(10)
-        mydb.commit()
-    
-    
+            time.sleep(3)
+            mydb.commit()
 
-print(bin_1.number)
-print(bin_1.capacity)
-print(bin_2.number)
-print(bin_2.capacity)
+    
+#MENU BASED PROGRAM
+#show data
+def analytics():
+    for i in range(1,no_of_bins+1):
+        table="bin"+str(i)
+        y="select * from {}".format(table)
+        curse.execute(y)
+        print("\n")
+        print(table)
+        print("   Time    Capacity")
+        for row in curse.fetchall():
+            print("\n")
+            for l in row:
+                print(l,end="\t")
+                mydb.commit()
+
+
+garbage_fill()
+analytics()
+curse.close()
+mydb.close()
 
 
 
